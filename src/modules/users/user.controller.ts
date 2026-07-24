2c57e97db6db34c5c58fbad2c39e400fafbe99d4
Thun Wityakon <thunwi@hotmail.com>
2026-07-24 20:32:59 +0700

    Update user.controller.ts

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserResponseDto } from './dto/user.dto';
+import { CreateUserDto, UserResponseDto } from './dto/user.dto';
 
-@Controller()
+@Controller('/users')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
     @Get()
     getUsers(): Promise<UserResponseDto[]> {
         return this.userService.getUsers();
+    }
+
+    @Get('/:id')
+    getUserById(@Param('id') id: number): Promise<UserResponseDto | null> {
+        return this.userService.getUserById(+id);
+    }
+
+    @Post()
+    createUser(@Body() data: CreateUserDto): Promise<UserResponseDto> {
+        return this.userService.createUser(data);
+    }
+
+    @Put('/:id')
+    updateUser(@Param('id') id: number, @Body() data: Partial<CreateUserDto>): Promise<UserResponseDto | null> {
+        return this.userService.updateUser(id, data);
+    }
+
+    @Delete('/:id')
+    deleteUser(@Param('id') id: number): Promise<UserResponseDto | null> {
+        return this.userService.deleteUser(id);
     }
 }
