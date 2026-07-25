bdf9b20b8b2ef022e70b8e98b5ca22b98b95f88a
Thun Wityakon <thunwi@hotmail.com>
2026-07-26 03:17:58 +0700

    Delete import

--- src/modules/users/user.service.spec.ts
+++ src/modules/users/user.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { PrismaService } from '../../database/prisma.service';
-import { UserResponseDto } from './dto/user.dto';
 
 describe('UserService', () => {
     let userService: UserService;
